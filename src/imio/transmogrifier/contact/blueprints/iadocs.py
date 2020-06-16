40383a7feb2735393ab8cf3f8cd05312cd75d488
Stephan Geulette <s.geulette@imio.be>
2020-06-16 16:10:16 +0200

    Added iadocs_creating_group_updater blueprint

--- src/imio/transmogrifier/contact/blueprints/iadocs.py
+++ src/imio/transmogrifier/contact/blueprints/iadocs.py
@@ -5,6 +5,7 @@ from collective.contact.importexport.utils import input_error
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from plone import api
+from Products.CMFPlone.utils import safe_unicode
 from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
@@ -31,4 +32,34 @@ class UseridUpdater(object):
                     item['userid'] = item['internal_number']
                 item['internal_number'] = None
 
+            yield item
+
+
+class CreatingGroupUpdater(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.portal = transmogrifier.context
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
+        self.ids = self.storage['ids']
+        creating_group = safe_unicode(transmogrifier['config'].get('creating_group', '').strip())
+        if not creating_group:
+            raise Exception(u'{}: You have to set creating_group value in config section !'.format(name))
+        reg = api.portal.get_registry_record('imio.dms.mail.browser.settings.IImioDmsMailConfig.contact_group_encoder')
+        if not reg:
+            raise Exception(u'{}: You have to activate the contact creating group option in iadocs config'.format(name))
+        # get imio.dms.mail.ActiveCreatingGroupVocabulary
+        from imio.dms.mail.vocabularies import ActiveCreatingGroupVocabulary
+        voc = ActiveCreatingGroupVocabulary()(self.portal)
+        creating_groups = {term.title: term.value for term in voc}
+        if creating_group not in creating_groups:
+            raise Exception(u"{}: given creating_group '{}' isn't an active creating group organization".format(
+                            name, creating_group))
+        self.creating_org = creating_groups[creating_group]
+
+    def __iter__(self):
+        for item in self.previous:
+            item['creating_group'] = self.creating_org
             yield item
